f116296ee23496886e4ac4a0e1bb323a0ecc5bcb
배찬혁 <new3930@gmail.com>
2023-03-10 21:16:31 +0900

    14

---
+++ src/component/Credits/Credits.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import style from './Credits.module.css'
+
+// https://www.themoviedb.org/t/p/w138_and_h175_face/tlAX3f82Mf5h0rznpVBVK7nD2om.jpg
+const Credits = ({credit}) => {
+  console.log(credit)
+  return (
+    <div className={style.credits_box}>
+      <div>cast</div>
+      <div className={style.credit_wrap}>
+        {credit.map((data) => {
+            return (
+              <div key={data.id} >
+                {data.profile_path &&
+                <div className={style.credit_title}>
+                  <img src={`https://www.themoviedb.org/t/p/w138_and_h175_face/${data.profile_path}`} />
+                  <div>{data.name}</div>
+                </div>
+                }
+              </div>
+            )
+          })}
+      </div>
+        
+    </div>
+  )
+}
+
+export default Credits
--- src/pages/PopularDetail/PopularDetail.js
+++ src/pages/PopularDetail/PopularDetail.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import Credits from '../../component/Credits/Credits'
 import DetailNav from '../../component/DetailNav/DetailNav'
 import Keyword from '../../component/Keyword/Keyword'
 import Reviews from '../../component/Reviews/Reviews'
@@ -14,19 +15,21 @@ const PopularDetail = () => {
   const [review, setReview] = useState();
   const [keywords, setKeywords] = useState();
   const [video, setVideo] = useState();
+  const [credit, setCredit] = useState([]);
   const [loading, setLoading] = useState(false);
   const params = useParams()
 
-  
   const Api = async () => {
     await axios.all(
       [axios.get(`https://api.themoviedb.org/3/movie/${params.id}?api_key=${API_KEY}&language=en-US`),
       axios.get(`https://api.themoviedb.org/3/movie/${params.id}/reviews?api_key=${API_KEY}&language=en-US&page=1`),
-      axios.get(`https://api.themoviedb.org/3/movie/${params.id}/videos?api_key=${API_KEY}&language=en-US`)])
+      axios.get(`https://api.themoviedb.org/3/movie/${params.id}/videos?api_key=${API_KEY}&language=en-US`),
+      axios.get(`https://api.themoviedb.org/3/movie/${params.id}/credits?api_key=${API_KEY}&language=en-US`)])
       .then((res) => {
         setDetail(res[0].data)
         setReview(res[1].data)
         setVideo(res[2].data)
+        setCredit(res[3].data.cast)
         setLoading(true)
       })
   }
@@ -57,6 +60,7 @@ const PopularDetail = () => {
           
         
       </div>
+        <Credits credit={credit}/>
         <Videos video={video}/>
         {/* <Reviews review={review}/> */}
     </div> : <div>로딩중....</div>}
